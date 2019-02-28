2102aa7f13d107edd47126c01ca086a3e163ddf9
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-02-28 11:08:33 +0100

    Update import_sirene.py

--- Imports/import_sirene.py
+++ Imports/import_sirene.py
@@ -7,13 +7,8 @@ with open('sirenerecode.csv', newline='') as csvfile:
     csvfile.readline()
     spamreader = csv.reader(csvfile, delimiter=';', quotechar='\"')
     for row in spamreader :
-        if "\"" in row[2] :  
-            recode=row[2].replace("\"", " ")
-            requete = "INSERT INTO SIRENE VALUES("
-            requete += "\""+row[0]+"\",\""+row[1]+"\",\""+recode+"\",\""+row[24]+"\",\""+row[27]+"\",\""+row[36]+"\",\""+row[42]+"\",\""+row[60]+"\",\""+row[61]+"\")"
-        else :
-            requete = "INSERT INTO SIRENE VALUES("
-            requete += "\""+row[0]+"\",\""+row[1]+"\",\""+row[2]+"\",\""+row[24]+"\",\""+row[27]+"\",\""+row[36]+"\",\""+row[42]+"\",\""+row[60]+"\",\""+row[61]+"\")"
+        requete = "INSERT INTO SIRENE VALUES("
+        requete += "\""+row[0]+"\",\""+row[1]+"\",\""+row[2].replace("\"", " ")+"\",\""+row[24]+"\",\""+row[27]+"\",\""+row[36]+"\",\""+row[42]+"\",\""+row[60]+"\",\""+row[61]+"\")"
         #print(requete)        
         c.execute(requete)
 conn.commit()
