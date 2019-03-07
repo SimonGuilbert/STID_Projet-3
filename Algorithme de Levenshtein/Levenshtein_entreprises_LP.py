7b9ee2abdfc04df00542f25c8ffebf336958122f
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-03-07 16:34:56 +0100

    Update Levenshtein_entreprises_LP.py

--- Algorithme de Levenshtein/Levenshtein_entreprises_LP.py
+++ Algorithme de Levenshtein/Levenshtein_entreprises_LP.py
@@ -16,71 +16,41 @@ import sqlite3
 conn = sqlite3.connect('data.db')
 c_LP = conn.cursor()
 
-seuil=0.1
-for row_LP in c_LP.execute('SELECT * FROM LP WHERE q6_14_6 IS NOT NULL'):
+seuil = 0.15
+for row_LP in c_LP.execute('SELECT * FROM LP WHERE q6_14_6 IS NOT NULL AND CODE_INSEE IS NOT NULL'):
+    print(row_LP[0])
     c_sirene = conn.cursor()
     distanceMinimum=1000
-    NIC = ""
-    NOM_ETAB = ""
-    APET700 = ""
-    SIREN = ""
-    requete = 'SELECT * FROM SIRENE WHERE CODE_INSEE = \"' + str(row_LP[8]) + '\"' + 'AND DEPET = \"' + str(row_LP[4]) + '\"'
+    if row_LP[8] not in [75056]:
+        requete = 'SELECT * FROM SIRENE WHERE CODE_INSEE = \"' + str(row_LP[8]) + '\"'
+    else:
+        requete = 'SELECT * FROM SIRENE WHERE DEPET = \"' + str(row_LP[4]) + '\"'
+    #print(requete)
+
+    nom_etablissement_LP = row_LP[7].lower()
+    nom_etablissement_LP = nom_etablissement_LP.strip()
+    nom_etablissement_LP = nom_etablissement_LP.replace("-", " ")
+    nom_etablissement_LP = nom_etablissement_LP.replace("\'", " ")
+   
     for row_sirene in c_sirene.execute(requete):
 
-        nom_etablissement_LP = row_LP[7].lower()
-        nom_etablissement_LP = nom_etablissement_LP.strip()
-        nom_etablissement_LP = nom_etablissement_LP.replace("-", " ")
-        nom_etablissement_LP = nom_etablissement_LP.replace("\'", " ")
+        for i in [2,5,7,8]:
+            Nom = row_sirene[i].lower()
+            Nom = Nom.strip()
+            Nom = Nom.replace("-", " ")
 
-        L1_NORMALISEE_sirene = row_sirene[2].lower()
-        L1_NORMALISEE_sirene = L1_NORMALISEE_sirene.strip()
-        L1_NORMALISEE_sirene = L1_NORMALISEE_sirene.replace("-", " ")
-
-        ENSEIGNE_sirene = row_sirene[5].lower()
-        ENSEIGNE_sirene = ENSEIGNE_sirene.strip()
-        ENSEIGNE_sirene = ENSEIGNE_sirene.replace("-", " ")
-
-        NOMEN_LONG_sirene = row_sirene[7].lower()
-        NOMEN_LONG_sirene = NOMEN_LONG_sirene.strip()
-        NOMEN_LONG_sirene = NOMEN_LONG_sirene.replace("-", " ")
-
-        SIGLE_sirene = row_sirene[8].lower()
-        SIGLE_sirene = SIGLE_sirene.strip()
-        SIGLE_sirene = SIGLE_sirene.replace("-", " ")
-
-        if levenshtein(nom_etablissement_LP, L1_NORMALISEE_sirene) < distanceMinimum :
-            distanceMinimum = levenshtein(nom_etablissement_LP, L1_NORMALISEE_sirene)
-            SIREN = row_sirene[0]
-            NIC = row_sirene[1]
-            NOM_ETAB = row_sirene[2]
-            APET700 = row_sirene[6]
-
-        if levenshtein(nom_etablissement_LP, ENSEIGNE_sirene) < distanceMinimum :
-            distanceMinimum = levenshtein(nom_etablissement_LP, ENSEIGNE_sirene)
-            SIREN = row_sirene[0]
-            NIC = row_sirene[1]
-            NOM_ETAB = row_sirene[5]
-            APET700 = row_sirene[6]
-
-        if levenshtein(nom_etablissement_LP, NOMEN_LONG_sirene) < distanceMinimum :
-            distanceMinimum = levenshtein(nom_etablissement_LP, NOMEN_LONG_sirene)
-            SIREN = row_sirene[0]
-            NIC = row_sirene[1]
-            NOM_ETAB = row_sirene[7]
-            APET700 = row_sirene[6]
-
-        if levenshtein(nom_etablissement_LP, SIGLE_sirene) < distanceMinimum :
-            distanceMinimum = levenshtein(nom_etablissement_LP, SIGLE_sirene)
-            SIREN = row_sirene[0]
-            NIC = row_sirene[1]
-            NOM_ETAB = row_sirene[8]
-            APET700 = row_sirene[6]
-
-        if distanceMinimum < seuil*len(row_lp[7]):
-            requete = 'UPDATE LP SET SIREN = \"' + SIREN + '\", '
-            requete += 'NIC = \"' + NIC + '\", NOM_ETAB = \"' + row_sirene[2] + '\", APET700 = \"' + APET700 + '\"'
-            print(requete)
-            #c_sirene.execute(requete)
+            if levenshtein(nom_etablissement_LP, Nom) < distanceMinimum :
+                distanceMinimum = levenshtein(nom_etablissement_LP, Nom)
+                SIREN = row_sirene[0]
+                NIC = row_sirene[1]
+                NOM_ETAB = row_sirene[2]
+                APET700 = row_sirene[6]
+    if distanceMinimum < seuil * len(row_LP[7]) :
+        requete = 'UPDATE LP SET SIREN = \"' + SIREN + '\", '
+        requete += 'NIC = \"' + NIC + '\", NOM_ETAB = \"' + row_sirene[2] + '\", APET700 = \"' + APET700 + '\"'
+        requete += ' WHERE CODE = \"' + row_LP[0] + '\"'
+        # print(requete)
+        c_sirene.execute(requete)
      
 conn.commit()
 conn.close()
