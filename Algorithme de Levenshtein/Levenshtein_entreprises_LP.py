8df3ef9da13409c001dd8239db0924d815ac7733
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-03-08 11:14:26 +0100

    Update Levenshtein_entreprises_LP.py

--- Algorithme de Levenshtein/Levenshtein_entreprises_LP.py
+++ Algorithme de Levenshtein/Levenshtein_entreprises_LP.py
@@ -21,6 +21,7 @@ for row_LP in c_LP.execute('SELECT * FROM LP WHERE q6_14_6 IS NOT NULL AND CODE_
     print(row_LP[0])
     c_sirene = conn.cursor()
     distanceMinimum=1000
+   
     if row_LP[8] not in [75056]:
         requete = 'SELECT * FROM SIRENE WHERE CODE_INSEE = \"' + str(row_LP[8]) + '\"'
     else:
@@ -31,9 +32,8 @@ for row_LP in c_LP.execute('SELECT * FROM LP WHERE q6_14_6 IS NOT NULL AND CODE_
     nom_etablissement_LP = nom_etablissement_LP.strip()
     nom_etablissement_LP = nom_etablissement_LP.replace("-", " ")
     nom_etablissement_LP = nom_etablissement_LP.replace("\'", " ")
-   
-    for row_sirene in c_sirene.execute(requete):
 
+    for row_sirene in c_sirene.execute(requete):
         for i in [2,5,7,8]:
             Nom = row_sirene[i].lower()
             Nom = Nom.strip()
@@ -45,12 +45,15 @@ for row_LP in c_LP.execute('SELECT * FROM LP WHERE q6_14_6 IS NOT NULL AND CODE_
                 NIC = row_sirene[1]
                 NOM_ETAB = row_sirene[2]
                 APET700 = row_sirene[6]
+        '''if distanceMinimum == 0:   
+            print(nom_etablissement_LP, NOM_ETAB)       
+            break
+        '''
     if distanceMinimum < seuil * len(row_LP[7]) :
         requete = 'UPDATE LP SET SIREN = \"' + SIREN + '\", '
-        requete += 'NIC = \"' + NIC + '\", NOM_ETAB = \"' + row_sirene[2] + '\", APET700 = \"' + APET700 + '\"'
+        requete += 'NIC = \"' + NIC + '\", NOM_ETAB = \"' + NOM_ETAB + '\", APET700 = \"' + APET700 + '\"'
         requete += ' WHERE CODE = \"' + row_LP[0] + '\"'
         # print(requete)
         c_sirene.execute(requete)
-     
 conn.commit()
 conn.close()
