626bc116e3ff15c992d1d9b20fae015c381b6959
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-03-08 12:27:44 +0100

    Create naf_MASTER.py

---
+++ NAF/naf_MASTER.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect('data.db')
+c_MASTER = conn.cursor()
+
+
+for row_MASTER in c_MASTER.execute('SELECT * FROM MASTER WHERE APET700 IS NOT NULL'):
+    c_naf = conn.cursor()
+    requete = 'SELECT * FROM NAF where APET700 = \'' + row_MASTER[15] + '\''
+    for row_naf in c_naf.execute(requete):
+        requete = 'UPDATE MASTER SET NIV1 = \'' + row_naf[1] + '\' WHERE CODE = \'' + row_MASTER[0] + '\''
+        print(requete)
+        #c_naf.execute(requete)
+     
+conn.commit()
+conn.close()
