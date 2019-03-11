3b6a171fd5b98bc38ed76ad91bf1fa897a4d0216
SimonSTID <43846246+SimonSTID@users.noreply.github.com>
2019-03-11 17:15:11 +0100

    Update naf_MASTER.py

--- NAF/naf_MASTER.py
+++ NAF/naf_MASTER.py
@@ -6,11 +6,11 @@ c_MASTER = conn.cursor()
 
 for row_MASTER in c_MASTER.execute('SELECT * FROM MASTER WHERE APET700 IS NOT NULL'):
     c_naf = conn.cursor()
-    requete = 'SELECT * FROM NAF where APET700 = \'' + row_MASTER[15] + '\''
+    requete = 'SELECT * FROM NAF where NIV5 = \'' + row_MASTER[15] + '\''
     for row_naf in c_naf.execute(requete):
         requete = 'UPDATE MASTER SET NIV1 = \'' + row_naf[1] + '\' WHERE CODE = \'' + row_MASTER[0] + '\''
-        print(requete)
-        #c_naf.execute(requete)
-     
+        #print(requete)
+        c_naf.execute(requete)
+    
 conn.commit()
 conn.close()
